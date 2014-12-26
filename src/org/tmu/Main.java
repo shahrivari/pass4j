007dfae555a3452d4b274c40ba241aa1d2cf8bcb
Saeed <saeed.shahrivari@gmail.com>
2014-12-27 01:08:21 +0330

    Refactored Class Hierarchy.

--- src/org/tmu/Main.java
+++ src/org/tmu/Main.java
@@ -11,23 +11,28 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        HashSet<SearchState> open_set = new HashSet<SearchState>();
-        HashSet<SearchState> close_set = new HashSet<SearchState>();
-        PriorityQueue<SearchState> Q = new PriorityQueue<SearchState>();
+        EightPuzzle rs = EightPuzzle.makeRandom(0);
+        rs = rs.moveLeft();
+        rs = rs.moveLeft();
+
+
+        HashSet<AStarState> open_set = new HashSet<AStarState>();
+        HashSet<AStarState> close_set = new HashSet<AStarState>();
+        PriorityQueue<AStarState> Q = new PriorityQueue<AStarState>();
         Q.add(EightPuzzle.makeRandom(1));
         while (!Q.isEmpty()) {
-            SearchState best = Q.poll();
-            if (best.getH() == 0.0)
+            AStarState best = Q.poll();
+            if (best.distanceToGoal() == 0.0)
                 break;
             System.out.println(best);
             close_set.add(best);
-            Collection<SearchState> neighbors = best.getAllNeighbors();
+            Collection<SearchState> neighbors = best.getNeighbors();
             for (SearchState neighbor : neighbors)
                 if (close_set.contains(neighbor) || open_set.contains(neighbor))
                     continue;
                 else {
-                    Q.add(neighbor);
-                    open_set.add(neighbor);
+                    Q.add((AStarState) neighbor);
+                    open_set.add((AStarState) neighbor);
                 }
         }
 
--- src/org/tmu/examples/EightPuzzle.java
+++ src/org/tmu/examples/EightPuzzle.java
@@ -1,51 +1,23 @@
 package org.tmu.examples;
 
+import org.tmu.AStarState;
 import org.tmu.SearchState;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Saeed on 12/23/2014.
  */
-public class EightPuzzle extends SearchState implements Comparable<EightPuzzle> {
+public class EightPuzzle extends AStarState implements Comparable<EightPuzzle> {
+    EightPuzzle parent = null;
     int[] arrangement = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
-    int actions = 0;
-    int displacement = 0;
+    int numberOfExpands = 0;
+    int displacements = 0;
 
     private EightPuzzle() {
-    }
-
-    public static EightPuzzle makeRandom(long seed) {
-        EightPuzzle result = new EightPuzzle();
-
-        Random rnd = new Random(seed);
-        for (int i = result.arrangement.length - 1; i > 0; i--) {
-            int index = rnd.nextInt(i + 1);
-            // Simple swap
-            int a = result.arrangement[index];
-            result.arrangement[index] = result.arrangement[i];
-            result.arrangement[i] = a;
-        }
-
-        result.setDisplacement();
-        return result;
-    }
-
-    @Override
-    public double getG() {
-        return actions;
-    }
-
-    @Override
-    public double getH() {
-        return displacement;
-    }
-
-    private void setDisplacement() {
-        displacement = 0;
-        for (int i = 0; i < arrangement.length; i++)
-            if (arrangement[i] != i)
-                displacement++;
     }
 
     @Override
@@ -68,8 +40,9 @@ public class EightPuzzle extends SearchState implements Comparable<EightPuzzle> 
     public EightPuzzle clone() {
         EightPuzzle new_state = new EightPuzzle();
         new_state.arrangement = arrangement.clone();
-        new_state.displacement = displacement;
-        new_state.actions = actions;
+        new_state.displacements = displacements;
+        new_state.numberOfExpands = numberOfExpands;
+        new_state.parent = parent;
         return new_state;
     }
 
@@ -90,18 +63,81 @@ public class EightPuzzle extends SearchState implements Comparable<EightPuzzle> 
         return builder.toString();
     }
 
-    EightPuzzle getLeft() {
-        EightPuzzle state = clone();
+    @Override
+    public int compareTo(EightPuzzle o) {
+        return Double.compare(fitness(), o.fitness());
+    }
+
+    @Override
+    public double distanceToGoal() {
+        return displacements;
+    }
+
+    @Override
+    public SearchState getParent() {
+        return parent;
+    }
+
+    @Override
+    public double distanceFromRoot() {
+        return numberOfExpands;
+    }
+
+    private int setDisplacement() {
+        displacements = 0;
+        for (int i = 0; i < arrangement.length; i++)
+            if (arrangement[i] != i)
+                displacements++;
+        return displacements;
+    }
+
+    public static EightPuzzle makeRandom(long seed) {
+        EightPuzzle result = new EightPuzzle();
+        Random rnd = new Random(seed);
+        for (int i = result.arrangement.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            int a = result.arrangement[index];
+            result.arrangement[index] = result.arrangement[i];
+            result.arrangement[i] = a;
+        }
+        result.setDisplacement();
+        return result;
+    }
+
+    private int z_index() {
         int z_index = 0;
         for (; z_index < arrangement.length; z_index++)
             if (arrangement[z_index] == 0)
                 break;
-
-        return state;
+        return z_index;
     }
 
+    public EightPuzzle moveLeft() {
+        EightPuzzle new_state = clone();
+        int z_index = z_index();
+        if (z_index % 3 == 0)
+            return null;
+        new_state.arrangement[z_index] = new_state.arrangement[z_index - 1];
+        new_state.arrangement[z_index - 1] = 0;
+        new_state.numberOfExpands++;
+        return new_state;
+    }
+
+    public EightPuzzle moveRight() {
+        EightPuzzle new_state = clone();
+        int z_index = z_index();
+        if (z_index % 3 == 2)
+            return null;
+        new_state.arrangement[z_index] = new_state.arrangement[z_index + 1];
+        new_state.arrangement[z_index + 1] = 0;
+        new_state.numberOfExpands++;
+        return new_state;
+    }
+
+
     @Override
-    public Collection<SearchState> getAllNeighbors() {
+    public List<SearchState> getNeighbors() {
         List<SearchState> neighbors = new ArrayList<SearchState>();
         for (int i = 0; i < arrangement.length; i++) {
             EightPuzzle state = clone();
@@ -109,14 +145,10 @@ public class EightPuzzle extends SearchState implements Comparable<EightPuzzle> 
             int a = state.arrangement[index];
             state.arrangement[index] = state.arrangement[i];
             state.arrangement[i] = a;
-            state.actions++;
+            state.numberOfExpands++;
             neighbors.add(state);
         }
         return neighbors;
     }
 
-    @Override
-    public int compareTo(EightPuzzle o) {
-        return Double.compare(getF(), o.getF());
-    }
 }
