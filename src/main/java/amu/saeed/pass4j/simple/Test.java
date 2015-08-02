e162f3d3c40ef9bb0b6258430a24aa07e0da97df
Saeed Shahrivari <saeed.shahrivari@gmail.com>
2015-08-02 20:08:12 +0430

    Some simple examples.

--- src/main/java/amu/saeed/pass4j/simple/Test.java
+++ src/main/java/amu/saeed/pass4j/simple/Test.java
@@ -1,14 +1,16 @@
 package amu.saeed.pass4j.simple;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Saeed on 7/25/2015.
  */
 public class Test {
     public static void main(String[] args) {
-        BubbleSortState goal = new BubbleSortState(new int[]{0, 1, 2, 3, 4, 5});
-        BubbleSortState root = new BubbleSortState(new int[]{5, 4, 3, 2, 1, 0});
+        int[] array = new int[]{0, 1, 2, 3, 4, 5};
+        BubbleSortState goal = new BubbleSortState(array);
+        BubbleSortState root = new BubbleSortState(shuffleArray(array));
         List<State> dfs = new DepthFirstSearch().solve(root, goal);
         System.out.println("DFS->");
         for (State s : dfs)
@@ -17,5 +19,19 @@ public class Test {
         System.out.println("BFS->");
         for (State s : bfs)
             System.out.println(s);
+    }
+
+    // Implementing Fisher–Yates shuffle
+    static int[] shuffleArray(int[] input) {
+        int[] ints = input.clone();
+        Random rnd = new Random();
+        for (int i = ints.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            int a = ints[index];
+            ints[index] = ints[i];
+            ints[i] = a;
+        }
+        return ints;
     }
 }
